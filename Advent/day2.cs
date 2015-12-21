446b6c0ba05770b8b8096ede60a3af9b113891c7
Simon Foster <funkysi1701@gmail.com>
2015-12-21 20:35:25 +0000

    day 2 question added

--- Advent/day2.cs
+++ Advent/day2.cs
@@ -13,6 +13,9 @@ namespace Advent
             //Day 2 Challenge
             day2 d2 = new day2();
             Console.WriteLine("Day 2");
+            Console.WriteLine("The elves are running low on wrapping paper, and so they need to submit an order for more.They have a list of the dimensions(length l, width w, and height h) of each present, and only want to order exactly as much as they need.");
+            Console.WriteLine("Fortunately, every present is a box(a perfect right rectangular prism), which makes calculating the required wrapping paper for each gift a little easier: find the surface area of the box, which is 2 * l * w + 2 * w * h + 2 * h * l.The elves also need a little extra paper for each present: the area of the smallest side.");
+            Console.WriteLine("All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?");
             Console.ReadLine();
             for (int i = 0; i < 1000; i++)
             {
@@ -21,6 +24,8 @@ namespace Advent
             }
             Console.WriteLine(d2.result);
             Console.ReadLine();
+            Console.WriteLine("The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about the length they need to order, which they would again like to be exact.");
+            Console.WriteLine("The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one face. Each present also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.");
             Console.WriteLine(d2.result2);
             Console.ReadLine();
         }
